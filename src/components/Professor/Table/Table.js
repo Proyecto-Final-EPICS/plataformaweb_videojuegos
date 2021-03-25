76ebd25540fbe3aa9b163159c352de9f9caae259
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2021-03-25 01:12:48 -0500

    Table with dummie values

--- src/components/Professor/Table/Table.js
+++ src/components/Professor/Table/Table.js
@@ -7,9 +7,28 @@ import './Table.scss';
 
 export default function Table(props){
     const {data} = props;
+    const datos = [
+        {
+            key:'1',
+            name: 'Diego',
+            age:21
+        }
+    ]
+    const columns = [
+        {
+            title:'Nombre',
+            dataIndex:'name',
+            key:'name'
+        },
+        {
+            title:'Edad',
+            dataIndex:'age',
+            key:'age'
+        }
+    ]
     console.log("Este es la data",data);
     return(
-        <Tb></Tb>
+        <Tb dataSource={datos} columns={columns}></Tb>
     );
 }
 //Columnas para tabla: inicio-final-nombre_est-nombre_juego
