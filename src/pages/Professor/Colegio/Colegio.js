edc34a54194f6385e52bb6fabc3d8cb083d339ba
Daalb <daalbor@uninorte.edu.co>
2021-04-08 00:06:11 -0500

    Passing parameters by id

--- src/pages/Professor/Colegio/Colegio.js
+++ src/pages/Professor/Colegio/Colegio.js
@@ -1,11 +1,13 @@
 //Liberias
 import React from 'react';
+import { useParams} from 'react-router-dom';
 
 //Estilos
 import './Colegio.scss';
 
-export default function Colegio(props){
-    console.log(props);
+export default function Colegio(){
+    const {colegio} = useParams();
+    console.log(colegio);
     return(
         <h1>Colegio Seleccionado</h1>
     );
