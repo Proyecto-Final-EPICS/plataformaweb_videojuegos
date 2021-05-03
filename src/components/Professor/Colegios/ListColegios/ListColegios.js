4ff3738926d7c9bc906258e92910233f536473d1
Daalb <daalbor@uninorte.edu.co>
2021-05-03 10:00:30 -0500

    Working in shools pages

--- src/components/Professor/Colegios/ListColegios/ListColegios.js
+++ src/components/Professor/Colegios/ListColegios/ListColegios.js
@@ -1,6 +1,6 @@
 //Liberias
 import React from 'react';
-import {List,Button} from 'antd';
+import {List,Button,Card} from 'antd';
 import {CaretUpOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 
@@ -24,13 +24,13 @@ function Colegios(props){
     return(
         
         <List
+            grid={{gutter: 16, column: 1}}
             className= "colegios"
             itemLayout= "horizontal"
             dataSource={colegios}
             renderItem={colegio => <Colegio
                 colegio = {colegio}
             />}
-        
         />
     );
 }
@@ -38,24 +38,34 @@ function Colegios(props){
 function Colegio(props){
     const {colegio} = props;
     return(
-        <List.Item
-            actions={[
-                <Link to ={`/home/colegios/${colegio.schoolName}`}>
-                    <Button type="primary"> 
-                        Entrar
-                    <CaretUpOutlined />
-                    </Button>
-                </Link>
+        <Card className="card">
+            <List.Item
+                actions={[
+                    <div className="card__content">
+                        <h1 className="card__content__title">
+                            {colegio.schoolName}
+                        </h1>
+        
+                        <Link to ={`/home/colegios/${colegio.schoolName}`}>
+                            <Button type="primary" className="card__button"> 
+                                Entrar
+                                <CaretUpOutlined />
+                            </Button> 
+                        </Link>
+                    </div>
+                    
+                ]}
                
-            ]}
+            >
+                {/* <div className="card__content">
+                    <List.Item.Meta
+                        title={`
+                            ${colegio.schoolName}
+                        `}
+                    />
+                </div> */}
+            </List.Item>
+        </Card>
         
-        
-        >
-            <List.Item.Meta
-                title={`
-                    ${colegio.schoolName}
-                `}
-            />
-        </List.Item>
     );
 }
