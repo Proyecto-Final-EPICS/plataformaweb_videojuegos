49f64783a318631d4851b7a87ec6df09b2476077
Daalb <daalbor@uninorte.edu.co>
2021-05-15 19:42:08 -0500

    Working in menu page

--- src/pages/Professor/Menu_Cards/Menu_Cards.js
+++ src/pages/Professor/Menu_Cards/Menu_Cards.js
@@ -9,24 +9,32 @@ export default function Menu_Cards(){
     return(
         <div className="content-menu">
             <Row>
-                <Col span={24}>
-                    TITULO
+                <Col span={8}>
+                </Col>
+                <Col span={8}>
+                    <div className="content-menu__titulo">
+                        <h1>
+                            SELECCIONE UNA OPCIÓN
+                        </h1>
+                    </div>
+                </Col>
+                <Col span={8}>
                 </Col>
             </Row>
 
             <Row>
                 <Col span={8}>
-                    <div>
+                    <div className="content-menu__card">
                         Opcion 1
                     </div>
                 </Col>
                 <Col span={8}>
-                    <div>
+                    <div className="content-menu__card">
                         Opcion 2
                     </div>
                 </Col>
                 <Col span={8}>
-                    <div>
+                    <div className="content-menu__card">
                         Opcion 3
                     </div>
                 </Col>
