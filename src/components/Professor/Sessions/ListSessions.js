702ded911d1898188d2924d65208dd8d3a5ef1c7
Daalb <daalbor@uninorte.edu.co>
2021-05-21 14:16:14 -0500

    Table fixed

--- src/components/Professor/Sessions/ListSessions.js
+++ src/components/Professor/Sessions/ListSessions.js
@@ -35,7 +35,6 @@ export default function ListSessions(props){
     //     columns.push(c);
     // })
 
-    console.log("Current",currentLevel);
 
     const getColumns = (sessions) =>{
         const columns = [
@@ -46,8 +45,9 @@ export default function ListSessions(props){
             }
         ]
 
+        let sw = true;
         sessions.forEach((session,index) =>{
-            if(session[0].level == nivel){
+            if(session[0].level == nivel && sw){
                 session[0].parameters.forEach(par =>{
                     const c = {
                         title: par.name,
@@ -56,31 +56,36 @@ export default function ListSessions(props){
                     }
                     columns.push(c);
                 })
+                sw = false;
             }
         })
-       
+        
+        console.log(columns);
         return columns;
     }
 
  
 
 
-    // const info = (sessions) =>{
-    //     const dataSource = []
-    //     sessions.forEach((session,index) => {
-    //         const data = {
-    //             key:0,
-    //             nro_de_sesion: index          
-    //         }
-    //         data.key = index;
-    //         data.nro_de_sesion = index + 1;
-    //         data.juego = session.Game.nameGame;
-    //         data.nro_de_fallas = session.Game.levels[0].parameters[0].value;
-    //         data.tiempo_de_nivel = session.Game.levels[0].parameters[1].value;
-    //         dataSource.push(data);
-    //     })
-    //     return dataSource;
-    // }
+    const info = (sessions) =>{
+        const dataSource = []
+        let it = 1;
+        sessions.forEach((session,index) => {
+            if(session[0].level == nivel){
+                const data = {
+                    key: index,
+                    nro_de_sesion: it   
+                }
+                session[0].parameters.forEach(par =>{
+                    data[par.name] = par.value
+                })
+                dataSource.push(data);
+                it++;
+            }
+            
+        })
+        return dataSource;
+    }
 
     return(
 
@@ -89,8 +94,8 @@ export default function ListSessions(props){
               Gráficos del nivel
             </Button>
 
-            {/* <Table dataSource={info(sessions)} columns={columns}/> */}
-            <Table columns={getColumns(sessions)}/>
+            <Table dataSource={info(sessions)} columns={getColumns(sessions)}/>
+            {/* <Table columns={getColumns(sessions)}/> */}
 
         </div>
     )
